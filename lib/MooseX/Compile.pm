f126c0d638f265fa94a0a81aefaecdc194db4c2d
Yuval Kogman <nothingmuch@woobling.org>
2007-12-10 22:42:57 +0000

    Initial version of MooseX::Compile

---
+++ lib/MooseX/Compile.pm
@@ -0,0 +1,324 @@
+#!/usr/bin/perl
+
+#use Carp ();
+#use Carp::Heavy ();
+#BEGIN { unshift @INC, sub { warn "loading file $_[1] at " . times . "\n"; warn Carp::longmess() } }
+
+package MooseX::Compile;
+
+use strict;
+use warnings;
+
+if ( $ENV{MX_COMPILE_PRELOAD_MOOSE} ) {
+    __PACKAGE__->load_moose();
+}
+
+our %known_pmc_files;
+
+sub import {
+    my ($self, @args) = @_;
+
+    my ( $class, $file ) = caller();
+
+    if ( $known_pmc_files{$file} ) {
+        return $self->load_pmc( $class, $file, @args );
+    } else {
+        require Check::UnitCheck;
+        Check::UnitCheck::unitcheckify(sub { $self->end_of_file_execution( $class, $file, @args ) });
+
+        require Moose;
+        shift; unshift @_, "Moose";
+        goto \&Moose::import;
+    }
+}
+
+sub load_classes {
+    my ( $self, @classes ) = @_;
+
+    foreach my $class ( @classes ) {
+        next if $class->can("meta");
+        ( my $file = "${class}.pm" ) =~ s{::}{/}g;
+        require $file;
+    }
+}
+
+sub sym ($$) {
+    my ( $sym, $type ) = @_;
+    bless \$sym, __PACKAGE__ . "::mangled::$type";
+}
+
+sub subref ($) {
+    sym($_[0], "subref");
+}
+
+sub end_of_file_execution {
+    my ( $self, $class, $file, @args ) = @_;
+    
+    if ( $ENV{MX_COMPILE_IMPLICIT_ANCESTORS} ) {
+        $self->compile_ancestors( $class );
+    }
+
+    $self->compile_class( $class, $file, @args );
+}
+
+sub compile_ancestors {
+    my ( $self, $class, %files ) = @_;
+
+    foreach my $superclass ( reverse $class->meta->linearized_isa ) {
+        next if $superclass eq $class;
+        $self->compile_class( $superclass, $files{$superclass} );
+    }
+}
+
+sub compile_class {
+    my ( $self, $class, $file, @args ) = @_;
+
+    unless ( defined $file ) {
+        ( my $class_file = "$class.pm" ) =~ s{::}{/}g;
+        $file = $INC{$class_file};
+    }
+
+    require Clone;
+    require Data::Visitor::Callback;
+
+    # refer to global items symbolically
+    my $meta = Data::Visitor::Callback->new(
+        ignore_return_values => 1,
+        "Moose::Meta::Class"     => "visit_ref",
+        "Moose::Meta::Method"    => "visit_ref",
+        "Moose::Meta::Attribute" => "visit_ref",
+        "Moose::Meta::TypeConstraint" => sub {
+            my ( $self, $constraint ) = @_;
+
+            if ( defined ( my $name = $constraint->name ) ) {
+                $_ = sym $name, "constraint";
+            } else {
+                warn "FOOO";
+            }
+        },
+        code   => sub {
+            my ( $self, $code ) = @_;
+
+            require Devel::Sub::Which;
+            if ( my $subname = Devel::Sub::Which::which($code) ) {
+                $subname =~ s/^Moose::Meta::Method::Accessor/$class/;
+                if ( $subname =~ /^Moose::([^:]+)$/ ) {
+                    my $method = $1;
+
+                    if ( $method eq 'meta' ) {
+                        $_ = subref "${class}::meta";
+                    } else {
+                        die "subname: $subname";
+                    }
+                } elsif ( $subname !~ /__ANON__$/ ) {
+                    $_ = subref $subname;
+                }
+            }
+        },
+    )->visit( Clone::clone($class->meta) );
+
+    require Storable;
+    Storable::nstore( $meta, MooseX::Compile->cached_meta_file_for_class( $class, $file ) );
+    #use YAML;
+    #$YAML::UseCode=1;
+    #YAML::DumpFile( MooseX::Compile->cached_meta_file_for_class($class, $file), $meta );
+
+    $self->write_pmc_file( $class, $file );
+}
+
+sub write_pmc_file {
+    my ( $self, $class, $file, @args ) = @_;
+
+    open my $pm_fh, "<", $file;
+    open my $pmc_fh, ">", "${file}c" or die "Can't write .pmc";
+
+    local $/;
+
+    my $pm = <$pm_fh>;
+
+    $pm =~ s/(?=__PACKAGE__->meta->make_immutable)/#/gm; # FIXME hack hack hack
+
+    print $pmc_fh "$1\n\n" if $pm =~ /^(\#\!.*)/; # copy shebang
+
+    require Data::Dump;
+    my $ISA = Data::Dump::dump(do { no strict 'refs'; @{"${class}::ISA"} });
+
+    my @methods = grep { $_->isa("Class::MOP::Method::Generated") } values %{ $class->meta->get_method_map };
+
+    require B::Deparse;
+    my $d = B::Deparse->new;
+
+    my $methods = join("\n\n", ( map { sprintf "*%s = sub %s;", $_->name => $d->coderef2text($_->body) } @methods ) );
+
+    print $pmc_fh <<PREAMBLE;
+# Register this file as a PMC so MooseX::Compile can hax0r it
+BEGIN {
+    require MooseX::Compile;
+    \$MooseX::Compile::known_pmc_files{+__FILE__} = 1;
+}
+
+# without use Moose we need to enable these manually
+use strict;
+use warnings;
+
+# stub the sugar
+{
+    package $class;
+
+    sub extends { }
+    sub has { }
+
+    sub meta { MooseX::Compile->load_cached_meta("$class", __FILE__) }
+}
+
+# try to approximate the time that Moose generated code enters the class
+# this presumes you didn't stick the moose sugar in a BEGIN { } block
+my \$__mx_compile_run_at_end = bless [ sub {
+    package $class;
+
+    our \@ISA = $ISA;
+    MooseX::Compile->load_classes(\@ISA);
+
+    # FIXME GIANT KLUDGE
+    my \$type_constraint = sub { 1 };
+    my \$attr; # used by the error if the constraint fails
+    my \$attrs = [ map { bless {}, "mockattr" } 1 .. 10 ];
+
+$methods
+
+} ], "MooseX::Compile::Scope::Guard";
+
+# line 1
+PREAMBLE
+
+    print $pmc_fh $pm;
+}
+
+sub load_moose {
+    require Class::MOP;
+    require Moose;
+    require Moose::Util::TypeConstraints;
+}
+
+sub load_pmc {
+
+}
+
+sub load_cached_meta {
+    my ( $self, $class, $file ) = @_;
+
+    my $meta = $self->inflate_cached_meta(
+        $self->load_raw_cached_meta($class, $file),
+        $class, $file
+    );
+
+    {
+        no strict 'refs';
+        no warnings 'redefine';
+        *{ "${class}::meta" } = sub { $meta };
+    }
+
+    Class::MOP::store_metaclass_by_name($class, $meta);
+}
+
+sub inflate_cached_meta {
+    my ( $self, $meta ) = @_;
+
+    require Class::Autouse;
+
+    Class::Autouse->autouse('Moose::Meta::Class');
+    Class::Autouse->autouse('Moose::Meta::Instance');
+
+    require Data::Visitor;
+
+    Data::Visitor::Callback->new(
+        ignore_return_values => 1,
+        object => sub {
+            my ( $self, $obj ) = @_;
+            #MooseX::Compile::load_classes(ref $obj);
+            Class::Autouse->autouse(ref $obj);
+        },
+        "Moose::Meta::Class"     => "visit_ref",
+        "Moose::Meta::Method"    => "visit_ref",
+        "Moose::Meta::Attribute" => "visit_ref",
+        "MooseX::Compile::mangled::constraint" => sub {
+            my ( $self, $sym ) = @_;
+            require Moose::Util::TypeConstraints;
+            $_ = Moose::Util::TypeConstraints::find_type_constraint($$sym);
+        },
+        "MooseX::Compile::mangled::subref" => sub {
+            my ( $self, $sym ) = @_;
+            no strict 'refs';
+            $_ = \&{ $$sym };
+        },
+    )->visit( $meta );
+
+    $meta;
+}
+
+sub load_raw_cached_meta {
+    my ( $self, $target, $file ) = @_;
+    
+    my $meta_file = $self->cached_meta_file_for_class($target, $file);
+
+    require Storable;
+    Storable::retrieve($meta_file);
+}
+
+sub cached_meta_file_for_class {
+    my ( $self, $target, $file ) = @_;
+
+    ( my $class_file = "$target.pm" ) =~ s{::}{/}g;
+
+    if ( $file eq ($INC{$class_file} || '') ) {
+        ( my $meta_file = $file ) =~ s/\.pm$/.mopc/;
+        #return file($meta_file);
+        return $meta_file;
+    } else { # try to support multiple classes per file
+        die "filename != class name ($file, $target, $INC{$class_file}";
+        ( my $meta_dir = $file ) =~ s/\.pm$//;
+        ( my $mangled_class = $target ) =~ s{::}{_}g;
+        # mkpath
+        require Path::Class;
+        return Path::Class::dir($meta_dir)->file( "$mangled_class.mopc" );
+    }
+}
+
+sub mockattr::type_constraint { bless {}, "mockconstraint" }
+
+sub mockconstraint::_compiled_type_constraint { sub { 1 } }
+
+sub MooseX::Compile::Scope::Guard::DESTROY {
+    my $self = shift;
+    $self->[0]->();
+}
+
+__PACKAGE__;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+MooseX::Compile - Moose ♥ .pmc
+
+=head1 SYNOPSIS
+
+    use Moose;
+
+	use MooseX::Compile; # defaults to maximal caching
+
+    # your moose class normally here
+
+=head1 DESCRIPTION
+
+L<MooseX::Compile> will attempt to use Perl's C<.pmc> mechanism to cache some
+of the meta computations performed by L<Moose> and L<Class::MOP> in order to
+cut load times.
+
+=cut
+
+
---
+++ t/point.t
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More 'no_plan';
+use Test::Exception;
+
+use lib "t/lib";
+
+use ok "Point";
+use ok "Point3D";
+
+my $point = Point->new(x => 1, y => 2);	
+
+isa_ok($point, 'Point');
+isa_ok($point, 'Moose::Object');
+
+is($point->x, 1, '... got the right value for x');
+is($point->y, 2, '... got the right value for y');
+
+$point->y(10);
+is($point->y, 10, '... got the right (changed) value for y');
+
+dies_ok {
+	$point->y('Foo');
+} '... cannot assign a non-Int to y';
+
+dies_ok {
+    $point->x(1000);
+} '... cannot assign to a read-only method';
+is($point->x, 1, '... got the right (un-changed) value for x');
+
+$point->clear();
+
+is($point->x, 0, '... got the right (cleared) value for x');
+is($point->y, 0, '... got the right (cleared) value for y');
+
+# check the type constraints on the constructor
+
+lives_ok {
+	Point->new(x => 0, y => 0);
+} '... can assign a 0 to x and y';
+
+dies_ok {
+	Point->new(x => 10, y => 'Foo');
+} '... cannot assign a non-Int to y';
+
+# Point3D
+
+my $point3d = Point3D->new({ x => 10, y => 15, z => 3 });
+isa_ok($point3d, 'Point3D');
+isa_ok($point3d, 'Point');
+isa_ok($point3d, 'Moose::Object');
+
+is($point3d->x, 10, '... got the right value for x');
+is($point3d->y, 15, '... got the right value for y');
+is($point3d->{'z'}, 3, '... got the right value for z');
+
+dies_ok {
+	$point3d->z;
+} '... there is no method for z';
+
+$point3d->clear();
+
+is($point3d->x, 0, '... got the right (cleared) value for x');
+is($point3d->y, 0, '... got the right (cleared) value for y');
+is($point3d->{'z'}, 0, '... got the right (cleared) value for z');
+
+dies_ok {
+	Point3D->new(x => 10, y => 'Foo', z => 3);
+} '... cannot assign a non-Int to y';
+
+dies_ok {
+	Point3D->new(x => 'Foo', y => 10, z => 3);
+} '... cannot assign a non-Int to x';
+
+dies_ok {
+	Point3D->new(x => 0, y => 10, z => 'Bar');
+} '... cannot assign a non-Int to z';
+
+# test some class introspection
+
+__END__
+
+can_ok('Point', 'meta');
+isa_ok(Point->meta, 'Moose::Meta::Class');
+
+can_ok('Point3D', 'meta');
+isa_ok(Point3D->meta, 'Moose::Meta::Class');
+
+isnt(Point->meta, Point3D->meta, '... they are different metaclasses as well');
+
